a354fdcd167949361936623c06505d18c450d6a8
Alex <baks750@mail.ru>
2020-04-24 19:30:07 +0300

    Убрал лишние импорты

--- src/main.js
+++ src/main.js
@@ -1,20 +1,12 @@
 import UserTitleComponent from './components/user-title.js';
 import FilterComponent from './components/filter.js';
 import StatisticComponent from './components/statistic.js';
-import FilmCardComponent from './components/film-card.js';
-import ShowMoreButtonComponent from './components/show-more-button.js';
-import FilmDetailsComponent from './components/film-details.js';
 import FilmsComponent from './components/films.js';
-import FilmsListComponent from './components/films-list.js';
-import FilmsListContainerComponent from './components/films-list-container.js';
-import NoFilmsComponent from './components/no-films.js';
-import MostCommentedFilmsListComponent from './components/most-commented-films-list.js';
-import TopRatedFilmsListComponent from './components/top-rated-films-list.js';
 import SortComponent from './components/sort.js';
 import FooterStatisticComponent from './components/footer-statistic.js';
 import {generateFilmCards} from './mock/film.js';
 import {generateFilters} from './mock/filter.js';
-import {RenderPosition, render, remove, removeChild, appendChild} from './utils/render.js';
+import {RenderPosition, render} from './utils/render.js';
 import PageControllerComponent from './controllers/page-control.js';
 
 const FILM_CARDS_COUNT = 25;
